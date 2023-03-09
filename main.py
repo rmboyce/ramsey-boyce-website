4159eba549e30f4e22d2497238f26b3c343797e6
Ramsey Boyce <ramseyboyce@gmail.com>
2023-03-08 16:23:19 -0800

    Clean up things

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
-from flask import redirect, render_template, request
+from flask import render_template
 
-from daily_crossword import *
-from javascript_demos import *
-from projects import *
+import daily_crossword
+import javascript_demos
+import projects
 from route_config import app
+from utils import force_https
 
-
-# Force https
-def force_https():
-    # Redirect any non-https requests to https
-    # Based largely on flask-sslify
-
-    if not app.debug:
-        app.config['SESSION_COOKIE_SECURE'] = True
-
-    criteria = [
-        app.debug,
-        request.is_secure,
-        request.headers.get('X-Forwarded-Proto', 'http') == 'https',
-    ]
-
-    if not any(criteria):
-        if request.url.startswith('http://'):
-            url = request.url.replace('http://', 'https://', 1)
-            #code = 302
-            code = 301
-            r = redirect(url, code=code)
-            return r
-
-app.before_request(force_https)
 
 #===================================
 #=              Index              =
 #===================================
+
+app.before_request(force_https)
 
 @app.route('/index')
 @app.route('/')
 def index():
     return render_template('custom_page.html', title='About Me', page_content='pages/index.html')
 
-
+# Error page
 @app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
+def page_not_found(_):
+    # Set the 404 status explicitly
     return render_template('404.html'), 404
 
 
