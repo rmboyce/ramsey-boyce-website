6ba7efb8be5185977516037c34bb2c5a61cb5d71
Ramsey Boyce <ramseyboyce@gmail.com>
2023-04-23 00:55:41 -0700

    Reorganize blueprints, refactoring

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import render_template
 
-import daily_crossword
-import javascript_demos
-import projects
+from daily_crossword import daily_crossword_pages
+from javascript_demos import javascript_demo_pages
+#from my import my_pages
+from projects import project_pages
 from route_config import app
 from utils import force_https
-
 
 #===================================
 #=              Index              =
 #===================================
 
 app.before_request(force_https)
+
 
 @app.route('/index')
 @app.route('/')
@@ -23,6 +24,15 @@ def index():
 def page_not_found(_):
     # Set the 404 status explicitly
     return render_template('404.html'), 404
+
+
+# Register blueprints for pages
+app.register_blueprint(daily_crossword_pages, url_prefix='/daily_crossword')
+app.register_blueprint(javascript_demo_pages, url_prefix='/javascript_demos')
+#app.register_blueprint(my_pages, url_prefix='/my')
+app.register_blueprint(project_pages, url_prefix='/projects')
+
+print(app.url_map)
 
 
 if __name__ == '__main__':
--- projects.py
+++ projects.py
@@ -1,24 +1,21 @@
 from flask import Blueprint, abort, render_template
 from jinja2 import TemplateNotFound
 
-from route_config import app
 from utils import title
 
 #===================================
 #=            Projects             =
 #===================================
 
-@app.route('/projects')
+project_pages = Blueprint('project_pages', __name__, template_folder='templates')
+
+@project_pages.route('/')
 def projects():
     return render_template('custom_page.html', title='My Projects', page_content='pages/projects.html')
 
-project_pages = Blueprint('project_pages', __name__, template_folder='templates')
-
-@project_pages.route('/projects/<page>')
+@project_pages.route('/<page>')
 def show(page):
     try:
         return render_template('custom_page.html', title=title(page), page_content=f'pages/projects/{page}.html')
     except TemplateNotFound:
         abort(404)
-
-app.register_blueprint(project_pages)
