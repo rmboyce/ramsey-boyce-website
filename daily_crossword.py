18770063dffc6222f019daf65d40affe94b283e8
Ramsey Boyce <ramseyboyce@gmail.com>
2024-02-11 15:15:32 -0800

    Fix requirements.txt, refactoring, add my pages

--- daily_crossword.py
+++ daily_crossword.py
@@ -13,17 +13,17 @@ daily_crossword_pages = Blueprint('daily_crossword_pages', __name__, template_fo
 
 @daily_crossword_pages.route('/today.puz')
 def current_crossword():
-    d = datetime.datetime.now(ZoneInfo('America/Los_Angeles'))
-    f = f'{str(d.year)}-{str(d.month)}-{str(d.day)}.puz'
+    DATE = datetime.datetime.now(ZoneInfo('America/Los_Angeles'))
+    FILE = f'{str(DATE.year)}-{str(DATE.month)}-{str(DATE.day)}.puz'
     try:
-        return send_from_directory('crosswords', f)
+        return send_from_directory('crosswords', FILE)
     except NotFound:
         # Return the default puzzle if there was an error getting today's puzzle
         return send_from_directory('crosswords', 'default.puz')
 
 @daily_crossword_pages.route('/')
 def daily_crossword():
-    h = Markup(f'''
+    HEAD = Markup(f'''
         <link rel="stylesheet" href="{url_for('static', filename='css/crosswordnexus.css')}">
 
         <script src="{url_for('static', filename='js/daily_crossword/crosswords.js')}"></script>
@@ -37,4 +37,4 @@ def daily_crossword():
             div.crossword {{ position: absolute; left: 0; top: 0; width: 100%; height: 100%; text-align: center; }}
         </style>
     ''')
-    return render_template('custom_page.html', title='Daily Crossword', head_html=h, page_content='pages/daily_crossword.html', squeeze=False)
+    return render_template('custom_page.html', title='Daily Crossword', head_html=HEAD, page_content='pages/daily_crossword.html', squeeze=False)
---
+++ my.py
@@ -0,0 +1,70 @@
+from time import sleep
+
+from dotenv import load_dotenv
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user
+from markupsafe import Markup
+import os
+from werkzeug.security import check_password_hash #, generate_password_hash
+
+from route_config import app
+from mysecrets import LINKS
+
+load_dotenv()
+
+app.secret_key = os.environ.get('SECRET_KEY')
+login_manager = LoginManager()
+login_manager.login_view = 'my_pages.login'
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    '''
+    Basic user class.
+    '''
+    def __init__(self, user_id) -> None:
+        super().__init__()
+        self.user_id = user_id
+
+    def get_id(self):
+        return self.user_id
+
+USER = User('user')
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id == 'user':
+        return USER
+
+
+my_pages = Blueprint('my_pages', __name__, template_folder='templates')
+
+@my_pages.route('/login')
+def login():
+    return render_template('login.html', title="Login")
+
+@my_pages.route('/api/login', methods=['POST'])
+def login_post():
+    sleep(1)
+    PASSWORD = request.form.get('password')
+    REMEMBER = bool(request.form.get('remember'))
+
+    if not check_password_hash(os.environ.get('PASSWORD'), PASSWORD):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('my_pages.login'))
+
+    login_user(USER, remember=REMEMBER)
+    return redirect(url_for('my_pages.show'))
+
+
+def generate_links():
+    html = ''
+    for link in LINKS:
+        html += f'<li>\n<a href="{link}" target="_blank">{link}</a>\n</li>\n'
+    return Markup(html)
+
+@my_pages.route('/links')
+@login_required
+def show():
+    links = generate_links()
+    return render_template('links.html', title='Links', links=links)
