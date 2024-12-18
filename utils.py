612d9e66b6f6c97e96f7a49cb66e0db6168c370d
Ramsey Boyce <ramseyboyce@gmail.com>
2024-12-17 23:24:31 -0800

    Format python files

--- utils.py
+++ utils.py
@@ -1,34 +1,36 @@
-from flask import redirect, request
+from flask import redirect, request, Response
 
 from route_config import app
 
 
-def title(page):
-    return page.replace('_', ' ').title()
+def title(page: str) -> str:
+    return page.replace("_", " ").title()
 
-def force_https():
+
+def force_https() -> Response | None:
     # Redirect any non-https requests to https
     # Based largely on flask-sslify
 
     if not app.debug:
-        app.config['SESSION_COOKIE_SECURE'] = True
+        app.config["SESSION_COOKIE_SECURE"] = True
 
     criteria = [
         app.debug,
         request.is_secure,
-        request.headers.get('X-Forwarded-Proto', 'http') == 'https',
+        request.headers.get("X-Forwarded-Proto", "http") == "https",
     ]
 
     if not any(criteria):
-        if request.url.startswith('http://'):
-            url = request.url.replace('http://', 'https://', 1)
-            #code = 302
+        if request.url.startswith("http://"):
+            url = request.url.replace("http://", "https://", 1)
+            # code = 302
             code = 301
             r = redirect(url, code=code)
             return r
 
-def clear_trailing():
+
+def clear_trailing() -> Response | None:
     # Clear trailing /s from urls
-    rp = request.path 
-    if rp != '/' and rp.endswith('/'):
+    rp = request.path
+    if rp != "/" and rp.endswith("/"):
         return redirect(rp[:-1])
